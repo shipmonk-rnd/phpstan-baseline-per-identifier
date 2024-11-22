da7e472c1d5dfcdc8023c765c294ccb53a54051e
Jan Nedbal <janedbal@gmail.com>
2024-11-22 10:47:59 +0100

    Fix php loader

--- tests/Rule/data/baselines-php/loader.php
+++ tests/Rule/data/baselines-php/loader.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-return array_merge(
+return array_merge_recursive(
     require __DIR__ . '/another.identifier.php',
     require __DIR__ . '/missing-identifier.php',
     require __DIR__ . '/sample.identifier.php',
