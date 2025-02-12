ea49c5914d7a83f23a25c72adab8cb38f483fe92
Jan Nedbal <janedbal@gmail.com>
2025-02-12 12:52:53 +0100

    IntegrationTest: ensure result cache is working (#36)

---
+++ tests/Integration/IntegrationTest.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types = 1);
+
+namespace ShipMonk\PHPStan\Baseline\Integration;
+
+use ShipMonk\PHPStan\Baseline\BinTestCase;
+use function array_map;
+use function file_put_contents;
+use function glob;
+use function is_dir;
+use function mkdir;
+
+final class IntegrationTest extends BinTestCase
+{
+
+    /**
+     * @dataProvider provideExtension
+     */
+    public function testResultCache(string $extension): void
+    {
+        $emptyConfig = $extension === 'php' ? '<?php return [];' : '';
+        $baselinesDir = 'cache/integration-test/baselines';
+        $baselinesDirAbs = __DIR__ . '/../../' . $baselinesDir;
+
+        if (!is_dir($baselinesDirAbs)) {
+            mkdir($baselinesDirAbs, 0777, true);
+        }
+
+        array_map('unlink', glob($baselinesDirAbs . '/*')); // @phpstan-ignore argument.type
+
+        // ensure dummy loader is present
+        file_put_contents($baselinesDirAbs . "/loader.$extension", $emptyConfig);
+
+        $cwd = __DIR__;
+        $phpstan = '../../vendor/bin/phpstan';
+        $split = '../../bin/split-phpstan-baseline';
+
+        $this->runCommand("$phpstan clear-result-cache -c $extension.neon", $cwd, 0);
+        $this->runCommand("$phpstan analyse -vv -c $extension.neon --generate-baseline=../../$baselinesDir/loader.$extension", $cwd, 0, null, 'Result cache is saved.');
+        $this->runCommand("php $split ../../$baselinesDir/loader.$extension", $cwd, 0, 'Writing baseline file');
+        $this->runCommand("$phpstan analyse -vv -c $extension.neon", $cwd, 0, null, 'Result cache restored. 0 files will be reanalysed.');
+
+        // cache should invalidate by editing the baseline
+        file_put_contents($baselinesDirAbs . "/method.notFound.$extension", $emptyConfig);
+
+        $this->runCommand("$phpstan analyse -vv -c $extension.neon", $cwd, 1, 'Call to an undefined method DateTime::invalid()');
+    }
+
+    /**
+     * @return iterable<array{string}>
+     */
+    public function provideExtension(): iterable
+    {
+        yield 'Neon' => ['neon'];
+        yield 'PHP' => ['php'];
+    }
+
+}
--- tests/SplitterTest.php
+++ tests/SplitterTest.php
@@ -3,18 +3,13 @@
 namespace ShipMonk\PHPStan\Baseline;
 
 use Nette\Neon\Neon;
-use PHPUnit\Framework\TestCase;
-use function fclose;
 use function file_put_contents;
 use function mkdir;
-use function proc_close;
-use function proc_open;
-use function stream_get_contents;
 use function sys_get_temp_dir;
 use function uniqid;
 use function var_export;
 
-final class SplitterTest extends TestCase
+final class SplitterTest extends BinTestCase
 {
 
     public function testBinaryWithNeon(): void
@@ -102,59 +97,6 @@ final class SplitterTest extends TestCase
                 ],
             ],
         ];
-    }
-
-    private function runCommand(
-        string $command,
-        string $cwd,
-        int $expectedExitCode,
-        ?string $expectedOutputContains = null,
-        ?string $expectedErrorContains = null
-    ): void
-    {
-        $desc = [
-            ['pipe', 'r'],
-            ['pipe', 'w'],
-            ['pipe', 'w'],
-        ];
-
-        $procHandle = proc_open($command, $desc, $pipes, $cwd);
-        self::assertNotFalse($procHandle);
-
-        /** @var list<resource> $pipes */
-        $output = stream_get_contents($pipes[1]); // @phpstan-ignore offsetAccess.notFound
-        $errorOutput = stream_get_contents($pipes[2]); // @phpstan-ignore offsetAccess.notFound
-        self::assertNotFalse($output);
-        self::assertNotFalse($errorOutput);
-
-        foreach ($pipes as $pipe) {
-            fclose($pipe);
-        }
-
-        $extraInfo = "Output was:\n" . $output . "\nError was:\n" . $errorOutput . "\n";
-
-        $exitCode = proc_close($procHandle);
-        self::assertSame(
-            $expectedExitCode,
-            $exitCode,
-            $extraInfo,
-        );
-
-        if ($expectedOutputContains !== null) {
-            self::assertStringContainsString(
-                $expectedOutputContains,
-                $output,
-                $extraInfo,
-            );
-        }
-
-        if ($expectedErrorContains !== null) {
-            self::assertStringContainsString(
-                $expectedErrorContains,
-                $errorOutput,
-                $extraInfo,
-            );
-        }
     }
 
 }
